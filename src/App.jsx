985f36eb52c3053261bd000c3efb4ef988d994d9
Sugeydi Magno Martinez <112962699+SugeydiMagno@users.noreply.github.com>
2023-03-07 09:21:51 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  const nombre = "Sugeydi Magno Martinez";
+
+  return (
+    <div>
+      <h3>Lista de Hobbies de {nombre}</h3>
+    <ol>
+      <li>Extrañar a mi Ex </li>
+      <li>Llorar</li>
+      <li>Comer</li>
+      <li>Estudiar </li>
+    </ol>
+    </div>
+  )
+}
+
+export default App
